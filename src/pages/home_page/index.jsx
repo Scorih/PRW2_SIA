af1293844576a2f7c2fade794f7a94073e452a30
Scorih <scorih.ivan@mail.ru>
2023-06-25 13:01:30 +0700

    Убрал повторение

--- src/pages/home_page/index.jsx
+++ src/pages/home_page/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const HomePage = () => {
-    return <div>home
+    return <div>
         <span style={{marginRight:20}}>home</span>
         <Link to={'/about_us'}>О руководстве</Link>
     </div>
