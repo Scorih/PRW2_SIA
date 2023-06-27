3b8434e2b799ee49608411c341eb18909e249f8d
Scorih <scorih.ivan@mail.ru>
2023-06-27 22:20:24 +0700

    Доделал верстку, подправил листы

---
+++ src/pages/header/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './style.scss'
+import { Link } from 'react-router-dom'
+
+export const HeaderPage = () => {
+    return <div className={"header"}>
+        <div className={'header__logo'}>
+            <img src={'/assets/logo.svg'} alt="logo" />
+        </div>
+        <div className={'header__menu'}>
+            <div><Link to={'/home_page'}>Главная</Link></div>
+            <div><Link to={'/light'}>Свет</Link></div>
+            <div><Link to={'/geometry'}>Геометрия</Link></div>
+            <div><Link to={'/other_entities'}>Прочие сущности</Link></div>
+            <div><Link to={'/optimization'}>Оптимизация</Link></div>
+            <div><Link to={'/about_us'}>О руководстве</Link></div>
+        </div>
+    </div>
+}
